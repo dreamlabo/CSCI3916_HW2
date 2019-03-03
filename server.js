1ef9382857f480e7416576aa71d85ab350c430cf
dreamlabo <toddlabo@gmail.com>
2019-03-03 13:00:06 -0700

    working on '/movies' section

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ router.post('/signin', function(req, res) {
 
 router.route('/movies') //////
     .get(function(req, res) {
-        res.status(200).send({message: 'GET movies', headers: req.headers, query: req.query, env: env.process.SECRET_KEY})
+        res.status(200).send({message: 'GET movies', headers: req.headers, query: req.query, env: ""})
 
         });
 
